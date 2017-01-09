521447e4f60b73fd7d79bf3747836d4398f0cfd3
LUMR <lumr@LUMRdeMacBook-Pro.local>
2017-01-09 23:40:17 +0800

    完成header的编写

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 //主页用的JS
-function nothing(){
-	
+function header_hover(mouse){
+	var header = document.getElementsByClassName('header_ul');
+	for (var i = 0; i < header.length; i++) {
+		header[i].className = "header_ul";
+	}
+	mouse.className = "current";
+}
+
+function header_hout(mouse){
+	var header = document.getElementsByClassName('header_ul');
+	mouse.className = "header_ul";
 }
