136e3bb8d050aaafa5bb2a1f620ce1159de2b35b
UMR L <UMR L>
2017-01-10 17:02:15 +0800

    add animate

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 //主页用的JS
+var photo_num = 1;
+setInterval("slide()",5000);
 function header_hover(mouse){
 	var header = document.getElementsByClassName('header_ul');
 	for (var i = 0; i < header.length; i++) {
@@ -10,4 +12,17 @@ function header_hover(mouse){
 function header_hout(mouse){
 	var header = document.getElementsByClassName('header_ul');
 	mouse.className = "header_ul";
+}
+
+function slide(){
+	var media = document.getElementById('media');
+	var li = media.getElementsByTagName('li');
+	var photo = new Array();
+	for (var i = 0; i < li.length; i++) {
+		photo[i] = 430*i;
+	}
+	$(media).animate({right:photo[photo_num++]},"slow");
+	if (photo_num == li.length) {
+		photo_num = 1;
+	}
 }
